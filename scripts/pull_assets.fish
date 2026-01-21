bb1ceb8b36e538b5618c0151995517300ebecc82
sloane <git@sloanelybutsurely.com>
2026-01-21 11:32:53 -0500

    use op to fetch config password

--- scripts/pull_assets.fish
+++ scripts/pull_assets.fish
@@ -5,6 +5,7 @@ set rclone_remote "cdn:sloanelybutsurely-media"
 
 rclone sync $rclone_remote $media_dir \
   --config rclone.conf \
+  --password-command "op item get owpj27pv36rdirzeriufzwyyyi --fields password --reveal" \
   --progress \
   --checksum \
   --exclude ".DS_Store" \
--- scripts/push_assets.fish
+++ scripts/push_assets.fish
@@ -5,6 +5,7 @@ set rclone_remote "cdn:sloanelybutsurely-media"
 
 rclone sync $media_dir $rclone_remote \
   --config rclone.conf \
+  --password-command "op item get owpj27pv36rdirzeriufzwyyyi --fields password --reveal" \
   --progress \
   --checksum \
   --exclude ".DS_Store" \
