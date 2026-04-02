194370fa415ee52ab28a1608233995ab98af2d76
sloane <git@sloanelybutsurely.com>
2026-04-02 08:57:03 -0400

    move about to home page, fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 bundle exec jekyll build
-rsync -rvz --delete _site/ vps.sloanelybutsurely.com:/var/www
+rsync -rvz --delete _site/ vps.sloanelybutsurely.com:/var/www/sloanelybutsurely.com
