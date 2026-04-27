0b3c6527511c0dcfeabe037c2f907466fa4ca057
sloane <sloane@sloanelybutsurely.com>
2026-04-27 11:02:14 -0400

    switch to astro

--- _drafts/simple-newletter.md
+++
@@ -1,4 +0,0 @@
----
-title: building a simple newsletter with n8n
-tags: email n8n automation
----
--- scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-bundle exec jekyll build
-rsync -rvz --delete _site/ vps.sloanelybutsurely.com:/var/www/sloanelybutsurely.com
